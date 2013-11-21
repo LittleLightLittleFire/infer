d9d0874117172f5d73ad2061aaa22e6561781345
lllf <littlelightlittlefire@gmail.com>
2013-11-21 19:55:58 +1100

    Add HBP for GPU

--- include/cuda/compose.h
+++ include/cuda/compose.h
@@ -2,11 +2,12 @@
 #define INFER_CUDA_COMPOSE_H
 
 #include "cuda/crf.h"
+#include <vector>
 
 namespace infer {
 namespace cuda {
 
-std::vector<unsigned> hbp(const unsigned layers, const unsigned iterations, const gpu_crf &crf);
+std::vector<unsigned> hbp(const unsigned layers, const unsigned iterations, const crf &crf);
 
 }
 }
--- src/cuda/compose.cu
+++ src/cuda/compose.cu
@@ -1,13 +1,44 @@
 #include "cuda/compose.h"
+#include "cuda/bp.h"
 
 namespace infer {
 namespace cuda {
 
-namespace {
+std::vector<unsigned> hbp(const unsigned layers, const unsigned iterations, const crf &crf) {
+    // we'll be breaking RAII here, since CUDA doesn't yet support C++11 and it is not
+    // worth the effort to hand roll a unique_ptr or use boost
+    // also there are no move semantics
 
-}
+    // what this means is that if we hit an exception we'll leak a lot of memory
 
-std::vector<unsigned> hbp(const unsigned layers, const unsigned iterations, const gpu_crf &crf) {
+    // generate a stack of CRFs
+    std::vector<const cuda::crf *> crfs;
+    for (unsigned i = 0; i < layers; ++i) {
+        crfs.push_back(i == 0 ? &crf : new cuda::crf(*crfs[i - 1], 0));
+    }
+
+    // create the method on the smallest layer
+    bp *m = new bp(*crfs.back());
+    m->run(iterations);
+
+    for (unsigned i = layers - 1; i-- > 0; ) {
+        bp *const new_m = new bp(*crfs[i], *m);
+        delete m;
+
+        m = new_m;
+        m->run(iterations);
+    }
+
+    // get the results
+    std::vector<unsigned> result = m->get_result();
+    delete m;
+
+    // clean up
+    for (unsigned i = 1; i < crfs.size(); ++i) {
+        delete crfs[i];
+    }
+
+    return result;
 }
 
 }
